b01f06bd770b17dd4c5ebe59fdc86f8d3e74fccf
niwong <nick@nickwong.io>
2019-04-13 03:20:57 -0700

    Update 2019-04-13T10:20:56.141Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,51 @@
 $(document).ready(function(){
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
 	$("select").formSelect();
+    var $form = $("form#orderForm")
+    var url = 'https://script.google.com/macros/s/AKfycbzlff4kD5HUprkvuAJJ8QqZzRMvfrAfJoWYd4DRSjoHQxrR1Ak/exec'
 
 	$("#addSticker").on("click", function() {
 		$("#secondField").css("display", "block");
 		$("#addSticker").addClass("disabled");
 	}) 
-       
+
+    // reliant on two stickers
+    $(".logo").on('click', function() {
+        if ($("#hipsterLogo").css("display") == 'none') {
+            $("#hipsterLogo").css("display", "inline-block");
+            $("#hypebeastLogo").css("display", "none"); 
+        } else if ($("#hipsterLogo").css("display") == 'inline-block') {
+            $("#hipsterLogo").css("display", "none");
+            $("#hypebeastLogo").css("display", "inline-block");
+        }
+    })
+
     $("form").on("submit", function(){
     	event.preventDefault();
-    	var first_name = $("#first_name").val();
-    	var last_name = $("#last_name").val();
-    	var email = $("#email").val();
 
-    	var stickerType = $("#stickerType").val();
-    	var quantity = $("#quantity").val();
-    	var sticker1 = [stickerType, quantity]
+        var stuff = $form.serializeObject();
+        stuff.date = today;
 
-    	var stickerType2 = $("#stickerType2").val();
-    	var quantity2 = $("#quantity2").val();
-    	var sticker2 = [stickerType2, quantity2];
 
-    	var order = [sticker1, sticker2];
-
-    	console.log(first_name, last_name, email, order);
+        console.log("let's post to sheets")
+        var jqxhr = $.ajax({
+          url: url,
+          method: "GET",
+          dataType: "json",
+          data: stuff,
+          success: function() {
+              console.log('form submit success')
+              $("#submitButton").removeClass("pink");
+              $("#submitButton").removeClass("lighten-3");
+              $("#submitButton").text("Order Placed");
+              $("#submitButton").addClass("disabled");
+          }
+        })
     })
 });
